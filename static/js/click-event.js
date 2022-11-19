761b0934fff6a1720012e51bae48ba13c4f9604a
AmetJunior <ametsejdaliev@gmail.com>
2022-11-19 20:45:28 +0300

    vse-gotovo

--- static/js/click-event.js
+++
@@ -1,9 +0,0 @@
-// export function event() {
-//     const btn_video = document.querySelector(".video_btn");
-//     console.log(btn_video);
-//     btn_video.addEventListener("click", checkboxClick)
-    
-//     function checkboxClick(e) {
-//         e.preventDefault();
-//     }
-// }
--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,4 @@
 
-
-
-import {sayHello} from "./carousel.js";
-sayHello();
-
-import { sayHello1 } from "./slider-background.js";
-sayHello1();
-
 import { Burger } from "./burger-menu.js";
 Burger();
 
